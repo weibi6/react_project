8963707b7d303e15a2ca02fbf3cbdfb76e3d09d7
weibi6 <1985838026@qq.com>
2019-11-14 15:16:42 +0800

    修改之后的

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Search from './search'
-import Main from './main'
+//import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
+
+/* 
+应用的根组件
+ */
 export default class App extends Component {
 
   render() {
     return(
-      <div className="container">
-        <Search />
-        <Main />
-      </div>
+      <BrowserRouter>
+          <Switch>
+{/*             <Route path='/login' component = { Login }></Route>
+ */}            <Route path='/' component = { Admin }></Route>
+          </Switch>
+      </BrowserRouter>
     )
   }
 }
