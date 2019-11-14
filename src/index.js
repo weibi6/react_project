9787ec85cec539c7b326b9f372292923741da086
weibi6 <1985838026@qq.com>
2019-11-14 15:17:11 +0800

    修改之后的

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-//import Main from './main';
-//import Search from './search';
-import * as serviceWorker from './serviceWorker';
+import storageUtils from './utils/storageUtils';
+import memoryUtils from './utils/memoryUtils';
+
+//读取local中保存的user, 保存到内存中
+const user = storageUtils.getUser()
+memoryUtils.user = user
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
